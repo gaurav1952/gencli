633d421e10f6a27adb8d3600e32cd321c33ee6ee
gaurav1952 <gaurav05655@gmail.com>
2024-08-03 13:32:35 +0530

    add web command

--- cmd/web.go
+++ cmd/web.go
@@ -11,7 +11,12 @@ var webCmd = &cobra.Command{
 	Use:   "web",
 	Short: "This command is use to create vanilla ",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		dirs := []string{"style", "script", "assest"}
+		dirs := []string{"style", "scripts", "assests"}
+		files := map[string]string{
+			"index.html":        "index.html",
+			"styles/style.css":  "styles/style.css",
+			"scripts/script.js": "scripts/script.js",
+		}
 
 		for _, dir := range dirs {
 			err := os.MkdirAll(dir, 0755)
@@ -20,6 +25,14 @@ var webCmd = &cobra.Command{
 				return err
 			}
 			fmt.Printf("Directory created succesfully: %s\n", dir)
+		}
+
+		for fileName, filePath := range files {
+			file, err := os.Create(filePath)
+			if err != nil {
+				fmt.Printf("Error crate file %s: %v\n ", fileName, err)
+			}
+			defer file.Close()
 		}
 
 		return nil
